bba65c619c2077bdc72566ba6016acc06cceff73
Muhammad Sohail Rehman <sohailrehman011@gmail.com>
2025-02-13 15:55:33 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
-  /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
       remotePatterns: [
         {
-          protocol: 'https',
-          hostname: 'cdn.sanity.io',
-          port: '',
-          pathname: '/**',
+          protocol: "https",
+          hostname: "cdn.sanity.io",
         },
       ],
     },
-  };
+  }
   
-  module.exports = nextConfig;
+  export default nextConfig
+  
   
